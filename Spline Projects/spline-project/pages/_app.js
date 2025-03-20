1c60c3c5aa7791fb6b78578a0a7596f5eb31317b
Gustavo Hammerschmidt <99229742+guhhammer@users.noreply.github.com>
2025-03-20 10:49:12 -0300

    Add files via upload

---
+++ Spline Projects/spline-project/pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+import { ThemeProvider } from "@material-tailwind/react";
+ 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
